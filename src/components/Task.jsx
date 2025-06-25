2f04f740de2604b5958fd7e2c49c9e6df20a378c
samikayaa <samikaya89@gmail.com>
2025-06-25 21:33:32 +0300

    c

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,12 +3,12 @@ import './task.css';
 
 const Task = ({ taskObj, onComplete }) => {
   return (
-    <div className="task rounded-md">
-      <h3>{taskObj.title}</h3>
+    <div className="task rounded-md bg-white leading-normal mt-4 shadow-[0_4px_5px_0_rgba(0,0,0,0.1)]">
+      <h3 className="text-lg text-[#c8781a]">{taskObj.title}</h3>
       <div className="deadline">
         son teslim: <span>{taskObj.deadline}</span>
       </div>
-      <p>{taskObj.description}</p>
+      <p className="">{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
           <span className="pill" key={p}>
